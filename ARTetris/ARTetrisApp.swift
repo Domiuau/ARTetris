c11740df190d2935aa71211a522b1a13c7a37afe
domiuau <domiuaunoob@outlook.com>
2025-11-11 02:20:34 -0300

    Initial Commit

---
+++ ARTetris/ARTetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARTetrisApp.swift
+//  ARTetris
+//
+//  Created by Guilherme Mateus on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ARTetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
